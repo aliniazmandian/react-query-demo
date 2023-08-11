05975979a90c00c8b7ee8d193706129cca2f7d52
ali niazmandian <aliniazmandian@gmail.com>
2023-08-11 04:56:26 +0330

    R-Q add fetching function

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,8 +1,29 @@
+import {useQuery} from "react-query";
+import axios from "axios";
 
 
 function RQSuperHeroesPage(props) {
+
+ const {isLoading , data} =  useQuery('super-heroes',()=>{
+    return axios.get('http://localhost:4000/superheroes')
+})
+
+    console.log(isLoading)
+
+    if (isLoading){
+        return <h2>Loading . . .</h2>
+    }
+
+
+
     return (
-        <div>Rq SuperHeroes Page</div>
+<div>
+    {data.data.map((hero)=>{
+        return  <div key={hero.id} >{hero.name}</div>
+    })}
+</div>
+
+
     );
 }
 
